b4bf3753153d29c450b720732966ce75dd1d7bf4
Daria <prudnikova.dashenka@gmail.com>
2020-04-03 20:40:20 +0300

    Add basic methods to PostsList

--- web/js/PostsList.js
+++ web/js/PostsList.js
@@ -3,4 +3,24 @@ class PostsList {
   constructor(posts) {
     this._posts = posts;
   }
+
+  static validate(post) {
+    console.log(`validating ${post}`);
+    return true;
+  }
+
+  get(id) {
+    return this._posts[id];
+  }
+
+  add(post) {
+    if (!PostsList.validate(post)) return false;
+    this._posts.push(post);
+    return true;
+  }
+
+  remove(id) {
+    this._posts = this._posts.filter(post => post.id !== id);
+    return this._posts;
+  }
 }
